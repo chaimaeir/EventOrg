9d9191a1cfa6f8a953def6a706437efcc90a60a2
chaimaeir <iraamane20@gmail.com>
2024-03-20 12:24:46 +0000

    msg

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+require('dotenv').config();
+
+const db=process.env.DATABASE_URL
+
+mongoose.connect(db)
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch((err)=>{ 
+        console.error("Failed to connect to MongoDB",err)
+        process.exit(1)
+
+}); 
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express=require( 'express')
+const database=require('./config/db')
+
+//com
+require('dotenv').config()
+
+const adminRoute= require('./routes/adminRoutes');
+const providerRoute = require('./routes/providerRoutes');
+const authRoute = require('./routes/authRoutes');
+const customerRoute =require ('./routes/customerRoutes');
+const eventsRoute = require('./routes/eventsRoutes');
+const feedbackRoute = require('./routes/feedbackRoutes');
+const notificationRoute = require('./routes/notificationRoutes');
+
+
+const  app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+/* app.use('/api/admin',adminRoute);
+app.use('/api/provider',providerRoute);
+app.use('/api/auth',authRoute);
+app.use('/api/customer',customerRoute);
+app.use('/api/events',eventsRoute);
+app.use('/api/feedback',feedbackRoute);
+app.use('/api/notification',notificationRoute);
+ */
+
+
+
+
+
+const port=process.env.PORT 
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port}`)
+})
