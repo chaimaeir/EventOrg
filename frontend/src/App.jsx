982844e6fa00ba124954bc3f8ab4abc099cefed6
chaimaeir <iraamane20@gmail.com>
2024-04-18 15:51:05 +0100

    sidebar admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,41 @@
-import "../src/apptest.css"
-function App() {
-  
-
+import React from 'react';
+import AdminSidebar from './components/dashboards/admin/AdminSidebar';
+import Navbar from './components/dashboards/admin/Navbar';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboards/admin/Dashboard';
+import Providers from './components/dashboards/admin/Providers';
+import Customers from './components/dashboards/admin/Customers';
+import Events from './components/dashboards/admin/Events';
+import Chat from './components/dashboards/admin/Chat';
+import Payment from './components/dashboards/admin/Payment';
+import './app.css'
+const App = ()=>{
   return (
     <>
-      <h1 className="text-xl font-bold text-red-700 text-center">EvOrg</h1>
-    </>
+  
+  <BrowserRouter>
+      <Routes>
+        
+        {/* admin routes */}
+        
+        <Route path='/' element={<AdminSidebar/>}/>
+
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Customers" element={<Customers />} />
+          <Route path="/Providers" element={<Providers />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Chat" element={<Chat />} />
+          <Route path="/Payment" element={<Payment />} />
+
+      </Routes>
+    </BrowserRouter>   
+  
+  </>
+
+
+
+
+
   )
 }
-
-export default App
+export default App;
---
+++ frontend/src/components/dashboards/admin/Providers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminSidebar from './AdminSidebar'
+
+function Providers() {
+  return (
+    <>
+      <AdminSidebar/>
+    </>
+  )
+}
+
+export default Providers
