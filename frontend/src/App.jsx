a7c085a32d38879a0087c56080d053788023591b
Bassam <arbenx6@gmail.com>
2024-05-14 16:41:32 +0100

    commit msg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 import "../src/apptest.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from "./components/auth/Login"
-import Register from './components/auth/Register';
-import RegisterProvider from './components/auth/RegisterProvider'
-import Home from "../src/Pages/Home/index";
-import LoginAdminForm from "./components/auth/Admin/loginadmin";
-//import Welcome from './components/auth/Welcome'
+import Login from "./Pages/auth/Login"
+import Register from './Pages/auth/Register';
+import RegisterProvider from './Pages/auth/RegisterProvider'
+import LoginAdminForm from "./Pages/auth/Admin/loginadmin";
+import SignUpAs from "./Pages/auth/SignUpAs";
+
 
   
   function App() {
     return (
       <BrowserRouter>
       <Routes>
-        <Route path="/Home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registerprovider" element={<RegisterProvider />} />
         <Route path="/loginAdmin" element={<LoginAdminForm />} />
-        {/* <Route path="/" element={<Welcome />} /> */}
-        
+        <Route path="/signUp" element={<SignUpAs />} />
       </Routes>
     </BrowserRouter>  
     );
